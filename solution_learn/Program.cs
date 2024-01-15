67fabb8e1b434542b8496ece8e2145afb686ec8b
DenSakharov <sakhirsp@gmail.com>
2024-01-16 00:37:34 +0300

    Add separate class with configuration for services

--- solution_learn/Program.cs
+++ solution_learn/Program.cs
@@ -13,20 +13,8 @@ builder.Services.AddSingleton<IStockSetvice, StockService>();
 ///как позволять клиентам интегрироваться с нашей апи
 //builder.Services.AddHttpClient<IStockHttpClient,StockHttpClient>();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-    ///сбор полного пространства имен
-    options.CustomSchemaIds(x => x.FullName);
-
-    ///добавляем путь к xml docs
-    var xmlFileName = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
-    var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
-    options.IncludeXmlComments(xmlFilePath);
-
-    /// добавляем 
-    options.OperationFilter<HeaderOperationFilter>();
-});
+/// добавление конфигурации в отдельный класс , переиспользование middleware
+builder.Host.AddInfrostructre();
 
 var app = builder.Build();
 
@@ -36,11 +24,6 @@ app.Map("/version", builder => builder.UseMiddleware<VersionMiddleware>());
 /// логирование запроса
 app.UseMiddleware<RequestLoggingMiddleWare>();
 
-///Swagger Midlleware
-app.UseSwagger();
-/// добавление страницы https://localhost:7119/swagger/index.html
-app.UseSwaggerUI();
-
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
@@ -49,3 +32,48 @@ app.UseEndpoints(endpoints =>
 });
 
 app.Run();
+
+public class SwaggerStartupFilter : IStartupFilter
+{
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        return app =>
+        {
+            ///Swagger Midlleware
+            app.UseSwagger();
+            /// добавление страницы https://localhost:7119/swagger/index.html
+            app.UseSwaggerUI();
+
+            //соблюдение последовательностей middleware
+            next(app);
+        };
+    }
+}
+
+public static class HostBuilderExtentions
+{
+    public static IHostBuilder AddInfrostructre(this IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            /// возможность работы с middleware
+            services.AddSingleton<IStartupFilter, SwaggerStartupFilter>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                ///сбор полного пространства имен
+                options.CustomSchemaIds(x => x.FullName);
+
+                ///добавляем путь к xml docs
+                var xmlFileName = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
+                var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+                options.IncludeXmlComments(xmlFilePath);
+
+                /// добавляем 
+                options.OperationFilter<HeaderOperationFilter>();
+            });
+        });
+        return builder;
+    }
+}
